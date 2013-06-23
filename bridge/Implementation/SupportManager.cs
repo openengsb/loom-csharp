d01a6cf6322125d75004f0d22ce857b63dad0d8f
frenzqse <e1029188@student.tuwien.ac.at>
2013-06-23 15:23:44 +0200

    [OPENENGSB-3639] Not all the dlls are copied to the outputdirectory (.Net Bridge)

---
+++ bridge/Implementation/SupportManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Org.Openengsb.Loom.CSharp.Bridge.Implementation
+{
+    public abstract class OpenEngSBImplementationSupportManager
+    {
+        public OpenEngSBImplementationSupportManager(Assembly assembly)
+        {
+            DomainFactoryProvider.addSupport(assembly);
+        }
+    }
+}
