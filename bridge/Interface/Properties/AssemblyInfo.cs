2995510c1ca518facbbc5510e02ac678ead03d0f
frenzqse <e1029188@student.tuwien.ac.at>
2013-12-28 01:43:38 +0100

    [OPENENGSB-3680] Update Version of the dependencies

--- bridge/Interface/Properties/AssemblyInfo.cs
+++ bridge/Interface/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyVersion("3.0.0.16")]
-[assembly: AssemblyFileVersion("3.0.0.16")]
-[assembly: AssemblyInformationalVersion("3.0.0.16-SNAPSHOT")]
+[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyFileVersion("1.0.2")]
+[assembly: AssemblyInformationalVersion("1.0.2")]
