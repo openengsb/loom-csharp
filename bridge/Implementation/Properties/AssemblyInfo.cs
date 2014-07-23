0244a80ae151549a615837e00b15565c05032ee6
frenzqse <e1029188@student.tuwien.ac.at>
2014-07-23 21:25:52 +0200

    [OPENENGSB-3781] Update dependencies and publish new Snapshot version (.Net Bridge)

--- bridge/Implementation/Properties/AssemblyInfo.cs
+++ bridge/Implementation/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("OpenEngSB")]
 [assembly: AssemblyProduct("Implemenation")]
-[assembly: AssemblyCopyright("Copyright © OpenEngSB 2013")]
+[assembly: AssemblyCopyright("Copyright © OpenEngSB 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,6 +49,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.0.0.18")]
-[assembly: AssemblyFileVersion("3.0.0.18")]
-[assembly: AssemblyInformationalVersion("3.0.0.18")]
+[assembly: AssemblyVersion("3.0.0.20")]
+[assembly: AssemblyFileVersion("3.0.0.20")]
+[assembly: AssemblyInformationalVersion("3.0.0.20")]
